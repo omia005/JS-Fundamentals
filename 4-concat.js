c53338e1f46da6089a3eed3f3f953a4de03be166
Evans Omia <evansomia005@gmail.com>
2025-09-15 00:01:35 +0400

    Updated 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,2 @@
 const [,, arg1, arg2] = process.argv;
-
-if (arg1 === undefined) {
-    console.log("No argument");
-} else if (arg2 === undefined) {
-    console.log(`${arg1} is undefined`);
-} else {
-    console.log(`${arg1} is ${arg2}`);
-}
+console.log(`${arg1 || 'undefined'} is ${arg2 || 'undefined'}`);
