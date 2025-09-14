abb1d26595a8ae0d278502d873fe5e567d3c4011
Evans Omia <evansomia005@gmail.com>
2025-09-15 00:18:51 +0400

    Updated 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -6,8 +6,4 @@ const [,, num1, num2] = process.argv;
 const a = parseInt(num1);
 const b = parseInt(num2);
 
-if (isNaN(a) || isNaN(b)) {
-    console.log("Invalid arguments");
-} else {
-    console.log(add(a, b));
-}
+console.log(add(a, b));
