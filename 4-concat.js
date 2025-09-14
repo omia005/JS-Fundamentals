6e45d99a259369b98fe66cedcf97fc6ddce3bdde
Evans Omia <evansomia005@gmail.com>
2025-09-14 23:56:09 +0400

    Created 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+const [,, arg1, arg2] = process.argv;
+if (arg1 !== undefined && arg2 !== undefined) {
+    console.log(`${arg1} is ${arg2}`);
+} else {
+    console.log("Not enough arguments provided");
+}
